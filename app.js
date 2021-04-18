62a28dee4a323bfaf506538baf87fd648ea40a76
nir vaknin <nirviko@gmail.com>
2021-04-19 00:38:12 +0300

    update app (3000 port)

--- app.js
+++ app.js
@@ -257,10 +257,9 @@ function ifLogin(req,res,next){
 
 
 //CONNECT TO SERVER//
-app.listen(3000,function(){
-    console.log("Server is running!");
-});
-
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 //END
 
 
